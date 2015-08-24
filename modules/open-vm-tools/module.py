10966a10eac4d369586a8c365a348c1df0f96c13
Timmons C. Player <timmons.player@spirent.com>
2015-08-24 12:47:11 -0400

    Update open-vm-tools module build.

--- modules/open-vm-tools/module.py
+++ modules/open-vm-tools/module.py
@@ -7,12 +7,5 @@ _base = '${OSV_BASE}/modules/open-vm-tools/install'
 usr_files = FileMap()
 usr_files.add(os.path.join(_base, 'usr/lib/libvmtools.so')) \
          .to('/usr/lib/libvmtools.so.0')
-usr_files.add(os.path.join(_base, 'usr/bin/vmtoolsd.so')) \
-         .to('vmtoolsd.so')
-usr_files.add(os.path.join(_base, 'usr/lib/open-vm-tools/plugins/vmsvc/libguestInfo.so')) \
-         .to('/usr/lib/open-vm-tools/plugins/vmsvc/libguestInfo.so')
 
 api.require('libglib')
-
-daemon = api.run_on_init('/vmtoolsd.so &!')
-default = daemon
