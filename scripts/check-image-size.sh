cbb2538398bde44087c9f475a290833922dd1966
Timmons C. Player <timmons.player@spirent.com>
2016-05-05 10:16:25 -0400

    scripts/check-image-size.sh: Use numeric {u,g}ids instead of text with ls
    This is more of a nit than anything.  This script cuts ls output, so spaces
    in user or group names trip it up.  Using numbers avoids that problem.

--- scripts/check-image-size.sh
+++ scripts/check-image-size.sh
@@ -5,12 +5,13 @@
 # This work is open source software, licensed under the terms of the
 # BSD license as described in the LICENSE file in the top-level directory.
 #
+
 if [ "$#" -ne 2 ]; then
     echo "usage: $0 file maxsize"
     exit 1
 fi
 
-if [ $(ls -l "$1" | cut -d " " -f 5) -ge "$2" ]; then
+if [ $(ls -ln "$1" | cut -d " " -f 5) -ge "$2" ]; then
     echo "$1 is greater than $2, needed to uncompress kernel"
     echo "Change arch/x64/boot16.S and arch/x65/lzloader.ld "
     echo " in order to have more available space for the kernel. "
