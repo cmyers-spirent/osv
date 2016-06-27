0e8e5ffee18034c4901d2f42bafa6671a9910afb
Timmons C. Player <timmons.player@spirent.com>
2016-06-27 15:58:34 -0400

    scripts/setup.py: add support for Ubuntu 16.04
    Ubuntu 16.04 replaces openjdk-7-jdk with openjdk-8-jdk.  Additionally,
    unzip, which is needed by the cli module build, is not included by
    default.

--- scripts/setup.py
+++ scripts/setup.py
@@ -111,7 +111,7 @@ class Ubuntu(object):
     name = 'Ubuntu'
     install = 'apt-get -y install'
     packages = ['build-essential', 'libboost-all-dev', 'genromfs', 'autoconf',
-                'libtool', 'openjdk-7-jdk', 'ant', 'qemu-utils', 'maven',
+                'libtool', 'ant', 'qemu-utils', 'maven',
                 'libmaven-shade-plugin-java', 'python-dpkt', 'tcpdump', 'gdb', 'qemu-system-x86',
                 'gawk', 'gnutls-bin', 'openssl', 'python-requests', 'p11-kit', 'g++-multilib',
                 'libssl-dev', 'libedit-dev', 'curl', 'libvirt-bin',
@@ -121,28 +121,35 @@ class Ubuntu(object):
     test_packages = ['libssl-dev', 'zip']
     ec2_post_install = None
 
+    class Ubuntu_16_04(object):
+        packages = ['openjdk-8-jdk', 'unzip']
+        ec2_packages = ['ec2-api-tools', 'awscli']
+        test_packages = []
+        ec2_post_install = None
+        version = '16.04'
+
     class Ubuntu_15_04(object):
-        packages = []
+        packages = ['openjdk-7-jdk']
         ec2_packages = ['ec2-api-tools', 'awscli']
         test_packages = []
         ec2_post_install = None
         version = '15.04'
 
     class Ubuntu_14_04(object):
-        packages = []
+        packages = ['openjdk-7-jdk']
         ec2_packages = ['ec2-api-tools', 'awscli']
         test_packages = []
         ec2_post_install = None
         version = '14.04'
 
     class Ubuntu_13_10(object):
-        packages = []
+        packages = ['openjdk-7-jdk']
         ec2_packages = []
         test_packages = []
         ec2_post_install = standard_ec2_post_install
         version = '13.10'
 
-    versions = [Ubuntu_15_04, Ubuntu_14_04, Ubuntu_13_10]
+    versions = [Ubuntu_16_04, Ubuntu_15_04, Ubuntu_14_04, Ubuntu_13_10]
 
 distros = [
            Debian(),
