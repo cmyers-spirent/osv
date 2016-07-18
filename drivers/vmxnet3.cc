1e4f709651c6c6573ad31ea83be012d2035c0803
Timmons C. Player <timmons.player@spirent.com>
2016-07-18 09:45:25 -0400

    drivers/vmxnet3.cc: More stability improvements
    * Handle both missing SOP and EOP descriptors in the receive loop.
    * Always leave at least one Tx descriptor available.  The driver
      appears to hang when there are no Tx descriptors available.

--- drivers/vmxnet3.cc
+++ drivers/vmxnet3.cc
@@ -537,7 +537,7 @@ int vmxnet3_txqueue::try_xmit_one_locked(void *req)
 
 int vmxnet3_txqueue::try_xmit_one_locked(vmxnet3_req *req)
 {
-    auto count = req->count;
+    auto count = req->count + 1;
     if (_avail < count) {
         gc();
         if (_avail < count)
@@ -830,17 +830,21 @@ void vmxnet3_rxqueue::receive()
             goto next;
         }
 
+        /* Check and handle SOP/EOP state errors */
+        if (rxcd->layout->sop && _m_currpkt_head) {
+            m_freem(_m_currpkt_head);
+            _m_currpkt_head = _m_currpkt_tail = nullptr;
+            stats.rx_drops++;
+        } else if (!rxcd->layout->sop && !_m_currpkt_head) {
+            stats.rx_drops++;
+            discard(rid, idx);
+            goto next;
+        }
+
         if (rxcd->layout->sop) {
             assert(rxd->layout->btype == btype::head);
             assert((idx % 1) == 0);
-            //assert(_m_currpkt_head == nullptr);
-
-            if (_m_currpkt_head) {
-                /* Guess the hypervisor lost a descriptor? */
-                m_freem(_m_currpkt_head);
-                _m_currpkt_head = _m_currpkt_tail = nullptr;
-                stats.rx_drops++;
-            }
+            assert(_m_currpkt_head == nullptr);
 
             if (length == 0) {
                 discard(rid, idx);
