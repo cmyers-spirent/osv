a3ed50a4a72b0f9c42779b9a3408097ce4d53061
Timmons C. Player <timmons.player@spirent.com>
2017-12-08 16:05:21 -0500

    BSD malloc: Update behavior of M_XENBLOCKFRONT arena
    The shared ring allocated from the M_XENBLOCKFRONT arean *must*
    be page aligned, hence force all M_XENBLOCKFRONT allocations
    to be page aligned.

--- bsd/sys/sys/malloc.h
+++ bsd/sys/sys/malloc.h
@@ -8,27 +8,7 @@
                    // Also differentiate from include/api/sys/param.h
 #include "priority.h"
 
-// But BSD malloc has extra parameters, that we will ignore. The third parameter though 
-// is important, since it can be asking us to zero memory. That one we honor
-static inline void *__bsd_malloc(size_t size, int flags)
-{
-    void *ptr = malloc(size);
-    if (!ptr)
-        return ptr;
-    if (flags & M_ZERO)
-        memset(ptr, 0, size);
-    return ptr;
-}
-
 #ifdef __FBSDID
-
-#ifndef __cplusplus
-
-#define malloc(x, y, z) __bsd_malloc(x, z)
-#define free(x, y) free(x)
-#define strdup(x, y) strdup(x)
-
-#else
 
 enum bsd_malloc_arena {
     M_WHATEVER,
@@ -39,9 +19,40 @@ enum bsd_malloc_arena {
     M_TEMP,
 };
 
-inline void* malloc(size_t size, int arena, int flags)
+// The BSD malloc has extra parameters, which we mostly ignore with two
+// exceptions:
+// 1) The M_XENBLOCKFRONT arena needs to be page aligned, since this allocation
+//    is shared with the Xen hypervisor
+// 2) Clients might want 0'd out memory, which we honor.
+inline void* __bsd_malloc(size_t size, int arena, int flags)
 {
-    return __bsd_malloc(size, flags);
+    void *ptr = NULL;
+    switch (arena) {
+    case M_XENBLOCKFRONT:
+        ptr = aligned_alloc(PAGE_SIZE, size);
+        break;
+    default:
+        ptr = malloc(size);
+    }
+
+    if (!ptr)
+        return ptr;
+    if (flags & M_ZERO)
+        memset(ptr, 0, size);
+    return ptr;
+}
+
+#ifndef __cplusplus
+
+#define malloc(x, y, z) __bsd_malloc(x, y, z)
+#define free(x, y) free(x)
+#define strdup(x, y) strdup(x)
+
+#else
+
+inline void *malloc(size_t size, int arena, int flags)
+{
+    return __bsd_malloc(size, arena, flags);
 }
 
 inline void free(void* obj, int flags)
